b01b9806cf7ae4183351be67efd346c33c20d724
Kosmonautilus <alexthelaveau@gmail.com>
2022-09-05 15:20:30 -0500

    Initial commit

---
+++ command_prompt.bat
@@ -0,0 +1,3 @@
+Call "C:\Program Files (x86)\Microsoft Visual Studio\2022\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+cmd
---
+++ src/my_mosaic.cpp
@@ -0,0 +1,8 @@
+
+void MyMosaicInit() {
+    
+}
+
+void MyMosaicUpdate() {
+
+}
